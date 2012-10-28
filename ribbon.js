eac1601dc8d0430a8e1663ae2ce49f2b749c3367
Stephen Stewart <stephen.stewart@carisenda.com>
2012-10-28 23:00:53 +0000

    refactor

--- ribbon.js
+++ ribbon.js
@@ -1,119 +1,126 @@
 var Art = function(canvas, config) {
 
-    this.cache = config || {};
+    config = config || {};
 
-    this.width = canvas.width;
-    this.height = canvas.height;
+    this.WIDTH = canvas.width;
+    this.HEIGHT = canvas.height;
 
-    this.radius = config.radius || 200;
-    this.amplitude = config.amplitude || 100;
-    this.cx = config.x || -this.width/2;
-    this.cy = config.y || this.height/2;
-    this.ROC = config.ROC || 0.0001;
-    this.rotation = config.rotation || 0.0004;
-    this.furr = config.furr || false;
-    this.complexity = config.complexity || 5;
+    this.RADIUS = config.radius || 200;
+    this.AMPLITUDE = config.amplitude || 100;
+    this.ROC = config.ROC || 0.0003;
+    this.ROTATION = config.rotation || 0.0004;
+    this.COMPLEXITY = config.complexity || 5;
 
-    this.points = this.createLine(this.complexity);
+    this.points = this.getPoints(this.COMPLEXITY);
 
-
-    this.TWO_PI = Math.PI * 2;
-
-    this.yPhase = Math.random() * this.TWO_PI;
-
-    this.drawCount = 0;
-    this.param = 0;
-    this.rotPhase = 0;
-    this.repeats = 1;
-    this.pinch = 1000;
+    this.count = 0;
+    this.PINCH = 5000;
+    this.GRAD_RADIUS0 = 0;
+    this.GRAD_RADIUS1= this.RADIUS;
 
     this.ctx = canvas.getContext('2d');
 
-    var grad = this.ctx.createRadialGradient(0,0,0,0,0,this.radius+50);
-    grad.addColorStop(0,"rgba(200,0,0,0.1)");
-    grad.addColorStop(1,"rgba(0,0,255,0.1)");
-
-    this.ctx.strokeStyle = grad;
-
-    this.ctx.setTransform(1,0,0,1,0,0)
-    this.ctx.clearRect(0,0,this.width,this.height);
+    this.init(true, -800);
 
 };
 
 Art.prototype = {
-    // todo move stuff to vars
-    render: function() {
-        if (!this.drawCount) {
-            this.ctx.setTransform(1, 0, 0, 1, 0, 0);
-            this.ctx.clearRect(0,0,this.width,this.height);
+
+    init: function (render, start) {
+        // clear canvas, reset transform
+        this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+        this.ctx.clearRect(0,0,this.WIDTH,this.HEIGHT);
+        // set styles
+        this.setStyle();
+        // call render
+        if (render && isNumber(start)) {
+            this._render(start);
+        } else if (!render) {
+            console.log("initialised, call render() to draw");
+        } else if (!isNumber(start)) {
+            console.log("start must be a valid x coord");
         }
 
-        var theta = 0,
-        x0, y0,
-        points = this.points;
-
-        for (j = 0; j < this.repeats; j++) {
-            this.drawCount++;
-
-
-            this.param += this.ROC;
-            this.cx += 1; // TODO add RTL option
-            this.cy += 0.004;
-            this.rotPhase += this.rotation;
-
-            //console.log(this.cx);
-
-            var easeFactor = this.easeInOut(this.param),
-
-            yOffset = this.amplitude * Math.sin( this.yPhase + this.drawCount * this.ROC * this.TWO_PI );
-
-            this.ctx.setTransform(1,0,0,1,this.cx,this.cy + yOffset);
-
-            this.ctx.beginPath();
-            for (var i = points.length;i--;) {
-                if (points[i-1]) {
-
-                    var x = points[i].x,
-                    y = points[i].y,
-                    xn = points[i-1].x, // next point
-                    yn = points[i-1].y;
-
-                    theta = this.TWO_PI * (x + easeFactor * ( xn - x )) + this.rotPhase;
-
-                    if (this.furr) {
-                        rad = (y + easeFactor * ( yn - y )) *
-                            (this.radius * Math.sin(this.drawCount/this.pinch ));
-                    } else {
-                        rad = (y + easeFactor * ( yn - y )) *
-                            // pinches the curve
-                            ((this.radius + (this.rotPhase * this.radius)) * Math.sin(this.drawCount/this.pinch)); 
-                    }
-
-                    x0 = this.radius + rad * Math.cos(theta);
-                    y0 = rad * Math.sin(theta);
-
-                    this.ctx.lineTo(x0,y0);
-                }
-            }
-            this.ctx.closePath();
-            this.ctx.stroke();
-        }
-        if (this.cx > (this.width + (10 * this.radius)) ) {
-            this.cx = -this.width/2;
-            this.cy = this.height/2; 
-            this.drawCount = 0;
-            this.rotPhase = 0;
-            this.param = 0;
-            this.theta = 0;
-            return;
-        } else {
-            this.render();
+        function isNumber(n) {
+            return !isNaN(parseFloat(n)) && isFinite(n);
         }
     },
+
+    setStyle:function() {
+        var grad = this.ctx.createRadialGradient(0,0,this.GRAD_RADIUS0,0,0,this.GRAD_RADIUS1);
+        grad.addColorStop(0,"rgba(200,0,0,0.1)");
+        grad.addColorStop(1,"rgba(0,0,255,0.1)");
+
+        this.ctx.strokeStyle = grad;
+    },
+
+    _drawPath: function(p, r) {
+        var x, y,
+        xnext, ynext,
+        x0,y0,
+        rad,
+        theta,
+        points = this.points,
+        easeFactor = this.easeInOut(p);
+
+        this.ctx.beginPath();
+        for (i = points.length; i-- && points[i-1];) {
+
+            x = points[i].x,
+            y = points[i].y,
+            xnext = points[i-1].x, // next point
+            ynext = points[i-1].y;
+
+            theta = (Math.PI * 2) * (x + easeFactor * ( xnext - x )) + r;
+
+            rad = (y + easeFactor * ( ynext - y )) *
+                // pinches the curve
+                ((this.RADIUS + (r * this.RADIUS)) * Math.sin(this.count/this.PINCH)); 
+
+            x0 = this.RADIUS + rad * Math.cos(theta);
+            y0 = rad * Math.sin(theta);
+
+            this.ctx.lineTo(x0,y0);
+        }
+        this.ctx.closePath();
+        this.ctx.stroke();
+    },
+
+    _render: function(dx, p, r) {
+
+        dy = 400;
+
+        p = p || 0;
+        r = r || 0;
+
+        this._drawPath(p, r);
+        this.count++;
+
+        dy += ~~(this.AMPLITUDE * Math.sin( this.count * this.ROC * (Math.PI * 2) ));
+
+        p += this.ROC;
+        r += this.ROTATION;
+
+        dx += 1; // TODO add RTL option
+
+
+        if (dx > (this.WIDTH)) {
+            this.reset();
+            return;
+        } else {
+            this.ctx.setTransform(1,0,0,1,dx, dy);
+            this._render(dx, p, r);
+        }
+    },
+
+    reset: function() {
+        this.count = 0;
+    },
+
     easeInOut: function(t) {
         return 0.5 - 0.5 * Math.cos( Math.PI * 2 * t); 
     },
-    createLine: function(n) {
+    getPoints: function(n) {
         // the initial point pair defining the line
         var line = [
             {x:0,y:1},
